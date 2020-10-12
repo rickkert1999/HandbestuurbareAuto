af78d45513a68c1ba5b973caa1fed3aae75b1e3e
Rick Peters <r.peters-ab@student.fontys.nl>
2020-10-12 15:22:38 +0200

    setup server

--- VrijeProject/LeapMotion/preliminaryInvestigation/LEAP/leapjs-master/lib/version.js
+++
@@ -1,7 +0,0 @@
-// This file is automatically updated from package.json by grunt.
-module.exports = {
-  full: '1.0.0',
-  major: 1,
-  minor: 0,
-  dot: 0
-}
---
+++ VrijeProject/LeapMotion/server/server.js
@@ -0,0 +1,47 @@
+//express npm
+const express = require("express");
+const app = express();
+
+//socket.io npm
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+//serialport npm
+const SerialPort = require("serialport");
+const Readline = require("@serialport/parser-readline");
+const port = new SerialPort("COM7");
+const parser = port.pipe(new Readline({ delimiter: "\r\n" }));
+
+const serverPort = 3000;
+
+app.use(express.static("public")); //set the public folder to static
+app.get("/", function (req, res) {
+  res.sendFile("public/index.html", { root: __dirname }); //your html file path
+});
+
+//Whenever someone connects this gets executed
+io.on("connection", function (socket) {
+  console.log("A user connected");
+
+  //sent data to the html file
+  parser.on("data", function (dataArduino) {
+    console.log(dataArduino); // Incomming data concole log
+    //sent the data for the arduino
+    socket.emit("sendData", {
+      description: dataArduino,
+    });
+  });
+
+  //Whenever someone disconnects this piece of code executed
+  socket.on("disconnect", function () {
+    console.log("A user disconnected");
+  });
+});
+
+//declare the port
+http.listen(3000, function () {
+  console.log("listening on *:3000");
+  console.log("listening on: http://localhost:" + serverPort);
+});
+
+//instal npm's: socket.io, express, serialport
