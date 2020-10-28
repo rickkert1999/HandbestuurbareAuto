78696129c42d11d9f755fa838523307522de1069
Rick Peters <r.peters-ab@student.fontys.nl>
2020-10-28 17:42:50 +0100

    Use leap to control arduino

--- VrijeProject/LeapMotion/arduino/arduinoNodeJs/arduinoNodeJs.ino
+++ VrijeProject/LeapMotion/arduino/arduinoNodeJs/arduinoNodeJs.ino
@@ -4,52 +4,52 @@ Servo servo_left;
 Servo servo_right;
 
 // Read vars
-String inputString = "";        // a string to hold incoming data
+String inputString = ""; // a string to hold incoming data
 
 void setup()
 {
-    // initialize serial:
-    Serial.begin(9600);
-    // define te servo signal pin
-    servo_left.attach (2);
-    servo_right.attach (3);
+  // initialize serial:
+  Serial.begin(9600);
+  // define te servo signal pin
+  servo_left.attach(2);
+  servo_right.attach(3);
 }
 
 void loop()
 {
-    // Recieve data from Node and write it to a String
-    while (Serial.available())
+  // Recieve data from Node and write it to a String
+  while (Serial.available())
+  {
+    char inChar = (char)Serial.read();
+    if (inChar == 'f')
     {
-        char inChar = (char)Serial.read();
-        if (inChar == 'f')
-        {
-          servo_left.write (100);
-          servo_right.write (80);
-        }
-        if (inChar == 'b')
-        {
-          servo_left.write (80);
-          servo_right.write (100);
-        }
-        if (inChar == 'l')
-        {
-          servo_left.write (80);
-          servo_right.write (80);
-        }
-        if (inChar == 'r')
-        {
-          servo_left.write (100);
-          servo_right.write (100);
-        }
-        if (inChar == 's')
-        {
-          servo_left.write (90);
-          servo_right.write (90);
-        }
-        else
-        {
-            inputString += inChar;
-        }
+      servo_left.write(100);
+      servo_right.write(80);
     }
-    delay(50); // give the Arduino some breathing room.
+    if (inChar == 'b')
+    {
+      servo_left.write(80);
+      servo_right.write(100);
+    }
+    if (inChar == 'l')
+    {
+      servo_left.write(80);
+      servo_right.write(80);
+    }
+    if (inChar == 'r')
+    {
+      servo_left.write(100);
+      servo_right.write(100);
+    }
+    if (inChar == 's')
+    {
+      servo_left.write(90);
+      servo_right.write(90);
+    }
+    else
+    {
+      inputString += inChar;
+    }
+  }
+  delay(50); // give the Arduino some breathing room.
 }
--- VrijeProject/LeapMotion/public/javascript/javascript.js
+++ VrijeProject/LeapMotion/public/javascript/javascript.js
@@ -1,3 +1,5 @@
+var socket = io();
+
 var buttons = document.querySelectorAll('.buttons');
 
 buttons.forEach(function(button) {
@@ -7,7 +9,51 @@ buttons.forEach(function(button) {
   });
 });
 
+//Tell the arduino what to do
 function dataSend(Direction){
   console.log(Direction);
   socket.emit("check", Direction);    
 };
+
+//Leap.loop uses browser's requestAnimationFrame
+var options = { enableGestures: true };
+
+let leftHand = "";
+let rightHand = "";
+
+//Main Leap loop
+Leap.loop(options, function (frame) {
+
+  //Loop for the hands
+  for (var i = 0, len = frame.hands.length; i < len; i++) {
+    //Make te hand
+    hand = frame.hands[i];
+  
+    //Get 2 var from your 2 hands
+    if (hand.type === "left") {
+      leftHand = hand.grabStrength
+    }
+    else {
+      rightHand = hand.grabStrength
+    }
+
+    //Tell the arduino what to do
+    if (frame.hands.length === 2) { 
+      if (rightHand < 0.5 && leftHand < 0.5){
+        socket.emit("check", "f");    
+      }
+      if (rightHand > 0.5 && leftHand > 0.5){
+        socket.emit("check", "b");    
+      }
+      if (rightHand < 0.5 && leftHand > 0.5){
+        socket.emit("check", "r");    
+      }
+      if (rightHand > 0.5 && leftHand < 0.5){
+        socket.emit("check", "l");    
+      }
+    } //Stop if there are no hands in the frame of the Leap Motion
+    else {
+      socket.emit("check", "s")
+    }    
+  }
+});
